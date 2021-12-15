a34387b0c3e2e219b100f86b4af40c2f6f5f0b3b
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2021-12-15 13:41:14 +0100

    Day Four - Part 2

--- aoc2021/day_four.py
+++ aoc2021/day_four.py
@@ -1,6 +1,21 @@
-from typing import Tuple, Optional
+import itertools
+from typing import Tuple, Iterable
 
 _BINGO_BOARD_LINES_SEPARATOR = ' '
+
+
+def _ichunk(iterable_to_chunk, chunk_size):
+    iterator_to_chunk = iter(iterable_to_chunk)
+
+    while True:
+        chunk = itertools.islice(iterator_to_chunk, chunk_size)
+
+        try:
+            first_element = next(chunk)
+        except StopIteration:
+            break
+
+        yield itertools.chain([first_element], chunk)
 
 
 def _index_to_x_y(number_of_columns: int, number_of_rows: int, index: int) -> Tuple[int, int]:
@@ -16,8 +31,8 @@ class BingoBoard:
         self._number_of_rows = number_of_rows
         self._numbers = numbers
         self._unmarked_numbers = list(numbers)
-        self._row_completion_counters = [0] * number_of_rows
-        self._column_completion_counters = [0] * number_of_columns
+        self._rows_completion = [0] * number_of_rows
+        self._columns_completion = [0] * number_of_columns
 
     def mark_number(self, number_to_mark: int):
         if number_to_mark not in self._numbers:
@@ -25,68 +40,79 @@ class BingoBoard:
 
         number_to_mark_index = self._numbers.index(number_to_mark)
 
-        x, y = _index_to_x_y(self._number_of_columns, self._number_of_rows, number_to_mark_index)
+        column, row = _index_to_x_y(self._number_of_columns, self._number_of_rows, number_to_mark_index)
 
-        self._column_completion_counters[x] = self._column_completion_counters[x] + 1
-        self._row_completion_counters[y] = self._row_completion_counters[y] + 1
+        self._columns_completion[column] = self._columns_completion[column] + 1
+        self._rows_completion[row] = self._rows_completion[row] + 1
 
         self._unmarked_numbers.remove(number_to_mark)
 
     def has_won(self) -> bool:
-        any_column_complete = any(column_counter for column_counter in self._column_completion_counters if column_counter == self._number_of_columns)
-        any_row_complete = any(row_counter for row_counter in self._row_completion_counters if row_counter == self._number_of_rows)
+        any_column_complete = any(column_counter for column_counter in self._columns_completion if column_counter == self._number_of_columns)
+        any_row_complete = any(row_counter for row_counter in self._rows_completion if row_counter == self._number_of_rows)
 
         return any_column_complete or any_row_complete
 
     def calculate_score(self, last_extracted_number: int) -> int:
         partial_score = 0
 
-        for not_marked_number in self._unmarked_numbers:
-            partial_score = partial_score + not_marked_number
+        for unmarked_number in self._unmarked_numbers:
+            partial_score = partial_score + unmarked_number
 
         return partial_score * last_extracted_number
 
 
-def _text_lines_to_bingo_board(number_of_columns: int, number_of_rows: int, text_lines: list[str]) -> BingoBoard:
+def _parse_bingo_boards_lines_chunk(number_of_columns: int, number_of_rows: int, bingo_board_lines_chunk: Iterable[str]) -> BingoBoard:
     numbers = []
 
-    for line in text_lines:
-        raw_row_numbers = line.split(_BINGO_BOARD_LINES_SEPARATOR)
-        row_numbers = [int(raw_number.strip()) for raw_number in raw_row_numbers if raw_number]
+    for bingo_board_line in bingo_board_lines_chunk:
+        raw_row_numbers = (raw_row_number.strip() for raw_row_number in bingo_board_line.split(_BINGO_BOARD_LINES_SEPARATOR))
+        row_numbers = [int(raw_row_number) for raw_row_number in raw_row_numbers if raw_row_number]
         numbers.extend(row_numbers)
 
     return BingoBoard(number_of_columns, number_of_rows, numbers)
 
 
-def parse_bingo_boards_file(number_of_columns: int, number_of_rows: int, file_path: str):
+def parse_bingo_boards_file(number_of_columns: int, number_of_rows: int, file_path: str) -> list[BingoBoard]:
     bingo_boards = []
 
     with open(file_path, mode='r') as bingo_boards_file:
-        bingo_board_text_lines = []
+        bingo_boards_file_stripped_lines = (line.strip() for line in bingo_boards_file)
+        bingo_boards_file_lines_with_content = (line for line in bingo_boards_file_stripped_lines if line)
+        bingo_boards_file_line_chunks = _ichunk(bingo_boards_file_lines_with_content, number_of_rows)
 
-        for bingo_board_line in bingo_boards_file:
-            clean_bingo_board_line = bingo_board_line.strip()
+        # Each chunk represents a Bingo Board and is made of number_of_rows text lines
+        for bingo_board_lines_chunk in bingo_boards_file_line_chunks:
+            if not bingo_board_lines_chunk:  # We ran out of chunks with content, so we assume we are at end of file
+                break
 
-            if clean_bingo_board_line:  # Bingo Boards are separated by empty lines
-                bingo_board_text_lines.append(clean_bingo_board_line)
-            else:
-                bingo_board = _text_lines_to_bingo_board(number_of_columns, number_of_rows, bingo_board_text_lines)
-                bingo_boards.append(bingo_board)
-                bingo_board_text_lines = []
-
-        # Let's check for any leftovers lines
-        if bingo_board_text_lines:
-            bingo_board = _text_lines_to_bingo_board(number_of_columns, number_of_rows, bingo_board_text_lines)
+            bingo_board = _parse_bingo_boards_lines_chunk(number_of_columns, number_of_rows, bingo_board_lines_chunk)
             bingo_boards.append(bingo_board)
 
     return bingo_boards
 
 
-def find_first_winning_board(numbers_to_draw: list[int], bingo_boards: list[BingoBoard]) -> Optional[BingoBoard]:
+def part_one(numbers_to_draw: list[int], bingo_boards: list[BingoBoard]) -> int:
     for number_drawn in numbers_to_draw:
         for bingo_board in bingo_boards:
             bingo_board.mark_number(number_drawn)
             if bingo_board.has_won():
-                return number_drawn, bingo_board
+                return bingo_board.calculate_score(number_drawn)
 
-    return None, None
+    return 0
+
+
+def part_two(numbers_to_draw: list[int], bingo_boards: list[BingoBoard]) -> int:
+    bingo_boards_that_won = []
+
+    for number_drawn in numbers_to_draw:
+        for bingo_board in bingo_boards:
+            if bingo_board in bingo_boards_that_won:
+                continue
+            bingo_board.mark_number(number_drawn)
+            if bingo_board.has_won():
+                bingo_boards_that_won.append(bingo_board)
+                if len(bingo_boards_that_won) == len(bingo_boards):
+                    return bingo_board.calculate_score(number_drawn)
+
+    return 0
--- test/test_day_four.py
+++ test/test_day_four.py
@@ -1,11 +1,142 @@
 import unittest
 
-from aoc2021.day_four import BingoBoard, parse_bingo_boards_file, find_first_winning_board
+from aoc2021.day_four import BingoBoard, parse_bingo_boards_file, part_one, part_two
+
+_PUZZLE_NUMBERS_TO_DRAW = [
+    38,
+    54,
+    68,
+    93,
+    72,
+    12,
+    33,
+    8,
+    98,
+    88,
+    21,
+    91,
+    53,
+    61,
+    26,
+    36,
+    18,
+    80,
+    73,
+    47,
+    3,
+    5,
+    55,
+    92,
+    67,
+    52,
+    25,
+    40,
+    56,
+    95,
+    9,
+    62,
+    30,
+    31,
+    85,
+    65,
+    14,
+    2,
+    78,
+    75,
+    15,
+    39,
+    87,
+    27,
+    58,
+    42,
+    60,
+    32,
+    41,
+    83,
+    51,
+    77,
+    10,
+    66,
+    70,
+    4,
+    37,
+    6,
+    89,
+    23,
+    16,
+    49,
+    48,
+    63,
+    94,
+    97,
+    86,
+    64,
+    74,
+    82,
+    7,
+    0,
+    11,
+    71,
+    44,
+    43,
+    50,
+    69,
+    45,
+    81,
+    20,
+    28,
+    46,
+    79,
+    90,
+    34,
+    35,
+    96,
+    99,
+    59,
+    1,
+    76,
+    22,
+    24,
+    17,
+    57,
+    13,
+    19,
+    84,
+    29
+]
 
 
 class DayFourTestCase(unittest.TestCase):
-    def test_BingoBoard_has_won_condition(self):
-        numbers_to_draw = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
+    def test_BingoBoard_has_won_condition_returns_true_if_BingoBoard_is_winning(self):
+        numbers_to_draw = [
+            7,
+            4,
+            9,
+            5,
+            11,
+            17,
+            23,
+            2,
+            0,
+            14,
+            21,
+            24,
+            10,
+            16,
+            13,
+            6,
+            15,
+            25,
+            12,
+            22,
+            18,
+            20,
+            8,
+            19,
+            3,
+            26,
+            1
+        ]
 
         bingo_board_numbers = [
             14, 21, 17, 24, 4,
@@ -24,26 +155,53 @@ class DayFourTestCase(unittest.TestCase):
         else:
             self.fail('Bingo board did not win')
 
-    def test_BingoBoard_solves_example_boards_correctly(self):
-        numbers_to_draw = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1]
-
-        bingo_boards = parse_bingo_boards_file(5, 5, 'day_four_sample_boards.txt')
-
-        winning_number_drawn, winning_bingo_board = find_first_winning_board(numbers_to_draw, bingo_boards)
-
-        self.assertEqual(winning_bingo_board, bingo_boards[2])
-        self.assertEqual(winning_number_drawn, 24)
-        self.assertEqual(winning_bingo_board.calculate_score(winning_number_drawn), 4512)
-
-    def test_BingoBoard_solves_puzzle_boards_correctly(self):
+    def test_day_four_part_one_solves_example_boards_correctly(self):
         numbers_to_draw = [
-            38, 54, 68, 93, 72, 12, 33, 8, 98, 88, 21, 91, 53, 61, 26, 36, 18, 80, 73, 47, 3, 5, 55, 92, 67, 52, 25, 40, 56, 95, 9, 62, 30, 31, 85,
-            65, 14, 2, 78, 75, 15, 39, 87, 27, 58, 42, 60, 32, 41, 83, 51, 77, 10, 66, 70, 4, 37, 6, 89, 23, 16, 49, 48, 63, 94, 97, 86, 64, 74, 82,
-            7, 0, 11, 71, 44, 43, 50, 69, 45, 81, 20, 28, 46, 79, 90, 34, 35, 96, 99, 59, 1, 76, 22, 24, 17, 57, 13, 19, 84, 29
+            7,
+            4,
+            9,
+            5,
+            11,
+            17,
+            23,
+            2,
+            0,
+            14,
+            21,
+            24,
+            10,
+            16,
+            13,
+            6,
+            15,
+            25,
+            12,
+            22,
+            18,
+            20,
+            8,
+            19,
+            3,
+            26,
+            1
         ]
 
-        bingo_boards = parse_bingo_boards_file(5, 5, 'day_four_puzzle_boards.txt')
+        bingo_boards = parse_bingo_boards_file(5, 5, 'inputs/day_four_sample_boards.txt')
 
-        winning_number_drawn, winning_bingo_board = find_first_winning_board(numbers_to_draw, bingo_boards)
+        score = part_one(numbers_to_draw, bingo_boards)
 
-        self.assertEqual(winning_bingo_board.calculate_score(winning_number_drawn), 58838)
+        self.assertEqual(score, 4512)
+
+    def test_day_four_part_one_solves_puzzle_boards_correctly(self):
+        bingo_boards = parse_bingo_boards_file(5, 5, 'inputs/day_four_puzzle_boards.txt')
+
+        score = part_one(_PUZZLE_NUMBERS_TO_DRAW, bingo_boards)
+
+        self.assertEqual(score, 58838)
+
+    def test_day_four_part_two_solves_puzzle_boards_correctly(self):
+        bingo_boards = parse_bingo_boards_file(5, 5, 'inputs/day_four_puzzle_boards.txt')
+
+        score = part_two(_PUZZLE_NUMBERS_TO_DRAW, bingo_boards)
+
+        self.assertEqual(score, 6256)

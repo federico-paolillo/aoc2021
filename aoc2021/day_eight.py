6b8b5d0b9ee5246d1fb911a9c5f4e31d2c84cde3
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2021-12-18 23:01:35 +0100

    Day Eight - Part 1 and Part 2 - Dirty

--- aoc2021/day_eight.py
+++ aoc2021/day_eight.py
@@ -10,6 +10,93 @@ class ScrambledSignal:
     digits: list[str] = field(default_factory=list)
 
 
+def _sort_string(string_to_sort):
+    return ''.join(sorted(string_to_sort))
+
+
+def _find_decoding_map(scrambled_signal):
+    patterns_left_to_decode = list(scrambled_signal.patterns)
+
+    number_1_pattern = next(pattern for pattern in patterns_left_to_decode if len(pattern) == 2)
+
+    patterns_left_to_decode.remove(number_1_pattern)
+
+    number_4_pattern = next(pattern for pattern in patterns_left_to_decode if len(pattern) == 4)
+    number_4_segments = set(number_4_pattern)
+
+    patterns_left_to_decode.remove(number_4_pattern)
+
+    number_7_pattern = next(pattern for pattern in patterns_left_to_decode if len(pattern) == 3)
+
+    patterns_left_to_decode.remove(number_7_pattern)
+
+    number_8_pattern = next(pattern for pattern in patterns_left_to_decode if len(pattern) == 7)
+    number_8_segments = set(number_8_pattern)
+
+    patterns_left_to_decode.remove(number_8_pattern)
+
+    # 3 is the only number that can be made with the same segments as number 7 plus two extra segments
+    number_3_pattern = next(pattern for pattern in patterns_left_to_decode if len(set(pattern).difference(set(number_7_pattern))) == 2)
+    number_3_segments = set(number_3_pattern)
+
+    patterns_left_to_decode.remove(number_3_pattern)
+
+    # 9 can only be made if it is using the same segments as number 3 and number 4
+    number_3_and_4_segments_set = number_3_segments | number_4_segments
+    number_9_pattern = next(pattern for pattern in patterns_left_to_decode if set(pattern) == number_3_and_4_segments_set)
+    number_9_segments = set(number_9_pattern)
+
+    patterns_left_to_decode.remove(number_9_pattern)
+
+    # 6 is the only number with 6 segments that differs from 9 by the only segment that is left out from 8 if we remove the segments of 9 from 8
+    diff_between_8_and_9 = number_8_segments - number_9_segments
+    number_6_pattern = next(pattern for pattern in patterns_left_to_decode if set(pattern) - number_9_segments == diff_between_8_and_9 and len(pattern) == 6)
+
+    patterns_left_to_decode.remove(number_6_pattern)
+
+    # 0 is the only number with 6 segments that is not the number 6. At this point it should be the only number left with 6 segments
+    number_0_pattern = next(pattern for pattern in patterns_left_to_decode if len(pattern) == 6)
+
+    patterns_left_to_decode.remove(number_0_pattern)
+
+    # 5 is like 9 except one segment
+    number_5_pattern = next(pattern for pattern in patterns_left_to_decode if len(number_9_segments - set(pattern)) == 1)
+
+    # 2 is the only number left
+    number_2_pattern = patterns_left_to_decode[0]
+
+    # Segments are sorted to make it possible to match a digit that might have the same segments but scrambled in a different order
+
+    return {
+        _sort_string(number_0_pattern): 0,
+        _sort_string(number_1_pattern): 1,
+        _sort_string(number_2_pattern): 2,
+        _sort_string(number_3_pattern): 3,
+        _sort_string(number_4_pattern): 4,
+        _sort_string(number_5_pattern): 5,
+        _sort_string(number_6_pattern): 6,
+        _sort_string(number_7_pattern): 7,
+        _sort_string(number_8_pattern): 8,
+        _sort_string(number_9_pattern): 9
+    }
+
+
+def decode_scrambled_signal_number(scrambled_signal):
+    decoding_map = _find_decoding_map(scrambled_signal)
+
+    decoded_digits = []
+
+    for encoded_digit in scrambled_signal.digits:
+        sorted_encoded_digit = _sort_string(encoded_digit)
+        if sorted_encoded_digit in decoding_map:
+            decoded_digit = str(decoding_map[sorted_encoded_digit])
+            decoded_digits.append(decoded_digit)
+
+    decoded_number_string = ''.join(decoded_digits)
+
+    return int(decoded_number_string)
+
+
 def parse_scrambled_signals_file(file_path: str) -> list[ScrambledSignal]:
     scrambled_signals = []
 
@@ -17,8 +104,8 @@ def parse_scrambled_signals_file(file_path: str) -> list[ScrambledSignal]:
         for line in scrambled_signals_file:
             raw_patterns_sequences, raw_digits_sequences = line.split(_SCRAMBLED_SIGNAL_FILE_LINE_PATTERN_AND_DIGITS_SEPARATOR)
 
-            raw_patterns = raw_patterns_sequences.split(_SCRAMBLED_SIGNAL_FILE_VALUES_SEPARATOR)
-            raw_digits = raw_digits_sequences.split(_SCRAMBLED_SIGNAL_FILE_VALUES_SEPARATOR)
+            raw_patterns = [raw_pattern for raw_pattern in raw_patterns_sequences.split(_SCRAMBLED_SIGNAL_FILE_VALUES_SEPARATOR) if raw_pattern]
+            raw_digits = [raw_digit for raw_digit in raw_digits_sequences.split(_SCRAMBLED_SIGNAL_FILE_VALUES_SEPARATOR) if raw_digit]
 
             patterns = [raw_pattern.strip() for raw_pattern in raw_patterns]
             digits = [raw_digit.strip() for raw_digit in raw_digits]
@@ -37,3 +124,13 @@ def part_one(scrambled_signals: list[ScrambledSignal]):
         number_of_1_4_7_8_digits = number_of_1_4_7_8_digits + sum(1 for digit in scrambled_signal.digits if len(digit) in [2, 3, 4, 7])
 
     return number_of_1_4_7_8_digits
+
+
+def part_two(scrambled_signals: list[ScrambledSignal]):
+    sum_of_all_digits = 0
+
+    for scrambled_signal in scrambled_signals:
+        number_decoded = decode_scrambled_signal_number(scrambled_signal)
+        sum_of_all_digits = sum_of_all_digits + number_decoded
+
+    return sum_of_all_digits
--- test/test_day_eight.py
+++ test/test_day_eight.py
@@ -1,9 +1,10 @@
 import unittest
 
-from aoc2021.day_eight import parse_scrambled_signals_file, part_one
+from aoc2021.day_eight import parse_scrambled_signals_file, part_one, ScrambledSignal, decode_scrambled_signal_number, part_two
 
 
 class DayEightTestCase(unittest.TestCase):
+
     def test_part_one_solves_example_scrambled_signals_correctly(self):
         signals = parse_scrambled_signals_file('inputs/day_eight_sample_signals.txt')
 
@@ -17,3 +18,36 @@ class DayEightTestCase(unittest.TestCase):
         number_of_1_4_7_8_digits = part_one(signals)
 
         self.assertEqual(488, number_of_1_4_7_8_digits)
+
+    def test_decode_scrambled_signal_digits_decodes_one_scrambled_signal_correctly(self):
+        scrambled_signal = ScrambledSignal(
+            [
+                'acedgfb',
+                'cdfbe',
+                'gcdfa',
+                'fbcad',
+                'dab',
+                'cefabd',
+                'cdfgeb',
+                'eafb',
+                'cagedb',
+                'ab'
+            ],
+            [
+                'cdfeb',
+                'fcadb',
+                'cdfeb',
+                'cdbaf'
+            ]
+        )
+
+        decoded_number = decode_scrambled_signal_number(scrambled_signal)
+
+        self.assertEqual(5353, decoded_number)
+
+    def test_part_two_solves_sample_scrambled_signals_correctly(self):
+        signals = parse_scrambled_signals_file('inputs/day_eight_sample_signals.txt')
+
+        sum_of_all_digits = part_two(signals)
+
+        self.assertEqual(488, sum_of_all_digits)

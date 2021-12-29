4a9666722fb504eb79d72de07f9c6ea5526c3f41
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2021-12-29 15:19:49 +0100

    Refactor and visualizations

--- aoc2021/day_nine.py
+++ aoc2021/day_nine.py
@@ -1,35 +1,38 @@
 import functools
 
 
+# Remember: all the points have their coordinates flipped, so expect y,x and not x,y
+# This is to remind me that we have a matrix that is an array of array where each array is a row
+
+
 def _risk_level(height_value):
     return height_value + 1
 
 
-def _get_adjacent_points_coordinates(y, x, height_map):
-    map_max_row_index = len(height_map) - 1
-    map_max_col_index = len(height_map[y]) - 1
+def _get_adjacent_points(y, x, height_map):
+    map_max_y = len(height_map) - 1
+    map_max_x = len(height_map[y]) - 1
 
-    # Handle corners first
-
+    # Corners
     if y == 0:
         if x == 0:
             return [
                 (y + 1, x),
                 (y, x + 1)
             ]
-        elif x == map_max_col_index:
+        elif x == map_max_x:
             return [
                 (y + 1, x),
                 (y, x - 1),
             ]
 
-    if y == map_max_row_index:
+    if y == map_max_y:
         if x == 0:
             return [
                 (y - 1, x),
                 (y, x + 1)
             ]
-        elif x == map_max_col_index:
+        elif x == map_max_x:
             return [
                 (y - 1, x),
                 (y, x - 1)
@@ -42,7 +45,7 @@ def _get_adjacent_points_coordinates(y, x, height_map):
             (y, x - 1),
             (y + 1, x)
         ]
-    elif y == map_max_row_index:
+    elif y == map_max_y:
         return [
             (y, x + 1),
             (y, x - 1),
@@ -55,7 +58,7 @@ def _get_adjacent_points_coordinates(y, x, height_map):
             (y - 1, x),
             (y + 1, x)
         ]
-    elif x == map_max_col_index:
+    elif x == map_max_x:
         return [
             (y, x - 1),
             (y - 1, x),
@@ -71,20 +74,21 @@ def _get_adjacent_points_coordinates(y, x, height_map):
     ]
 
 
-def _low_points_coordinates(height_map):
-    low_points_coordinates = []
+def _low_points(height_map):
+    low_points = []
 
     for y, columns in enumerate(height_map):
-        for x, heightmap_point in enumerate(columns):
-            adjacent_points_coordinates = _get_adjacent_points_coordinates(y, x, height_map)
-            adjacent_points = (height_map[y][x] for (y, x) in adjacent_points_coordinates)
-            smallest_adjacent_point = min(adjacent_points)
+        for x, point_height in enumerate(columns):
+            adjacent_points = _get_adjacent_points(y, x, height_map)
 
-            if heightmap_point < smallest_adjacent_point:
-                low_point_coordinates = (y, x)
-                low_points_coordinates.append(low_point_coordinates)
+            adjacent_points_heights = (height_map[y][x] for (y, x) in adjacent_points)
 
-    return low_points_coordinates
+            smallest_adjacent_point_height = min(adjacent_points_heights)
+
+            if point_height < smallest_adjacent_point_height:
+                low_points.append((y, x))
+
+    return low_points
 
 
 def _is_out_of_bounds(y, x, height_map):
@@ -107,8 +111,8 @@ def _is_out_of_bounds(y, x, height_map):
     return False
 
 
-def _is_inside_basin(y, x, visited_coordinates, height_map):
-    if (y, x) in visited_coordinates:
+def _is_inside_basin(y, x, visited_points, height_map):
+    if (y, x) in visited_points:
         return False
 
     if _is_out_of_bounds(y, x, height_map):
@@ -122,7 +126,7 @@ def _is_inside_basin(y, x, visited_coordinates, height_map):
 
 # Span Filling flood fill, stolen from: https://en.wikipedia.org/wiki/Flood_fill#Span_Filling
 def _scan_for_basin(basin_lowest_point_y, basin_lowest_point_x, height_map):
-    basin_height_values = []
+    basin_points = []
 
     coordinates_to_visit = [(basin_lowest_point_y, basin_lowest_point_x)]
     visited_coordinates = set()
@@ -133,38 +137,42 @@ def _scan_for_basin(basin_lowest_point_y, basin_lowest_point_x, height_map):
         span_x_start = seed_point_x
         span_x_end = seed_point_x
 
-        # Move to the left of the seed point, remember what we visit and add it to the basin coordinates
+        # Scan a line to the left of the seed point, remember what we visit and add it to the basin coordinates
         while _is_inside_basin(seed_point_y, span_x_start - 1, visited_coordinates, height_map):
             span_x_start = span_x_start - 1
             visited_coordinates.add((seed_point_y, span_x_start))
-            basin_height_values.append(height_map[seed_point_y][span_x_start])
+            basin_points.append((seed_point_y, span_x_start))
 
-        # Move to the right of the seed point, remember what we visit and add it to the basin coordinates
+        # Scan a line to right of the seed point, remember what we visit and add it to the basin coordinates
         while _is_inside_basin(seed_point_y, span_x_end, visited_coordinates, height_map):
             visited_coordinates.add((seed_point_y, span_x_end))
-            basin_height_values.append(height_map[seed_point_y][span_x_end])
+            basin_points.append((seed_point_y, span_x_end))
             span_x_end = span_x_end + 1
 
-        # Try to scan a line of the same length below the current line
+        # Once we finish scanning left and right we are left with a start and end of a line
+
+        # Try to scan the same line below the current line
         for line_above_x in range(span_x_start, span_x_end):
             if _is_inside_basin(seed_point_y + 1, line_above_x, visited_coordinates, height_map):
                 coordinates_to_visit.append((seed_point_y + 1, line_above_x))
 
-        # Try to scan a line of the same length above the current line
+        # Try to scan the same line above the current line
         for line_below_x in range(span_x_start, span_x_end):
             if _is_inside_basin(seed_point_y - 1, line_below_x, visited_coordinates, height_map):
                 coordinates_to_visit.append((seed_point_y - 1, line_below_x))
 
-    return basin_height_values
+    return basin_points
 
 
 def part_one(height_map):
-    return sum(_risk_level(height_map[y][x]) for (y, x) in _low_points_coordinates(height_map))
+    return sum(_risk_level(height_map[y][x]) for (y, x) in _low_points(height_map))
 
 
 def part_two(height_map):
-    low_points_coordinates = _low_points_coordinates(height_map)
-    basins = sorted([_scan_for_basin(y, x, height_map) for (y, x) in low_points_coordinates], key=len)
-    largest_three_basins = basins[-3:]
+    low_points = _low_points(height_map)
 
-    return functools.reduce(lambda basins_len_multiplied, basin: basins_len_multiplied * len(basin), largest_three_basins, 1)
+    basins_by_size = sorted([_scan_for_basin(y, x, height_map) for (y, x) in low_points], key=len)
+
+    largest_three_basins = basins_by_size[-3:]
+
+    return functools.reduce(lambda basins_size_multiplied, basin: basins_size_multiplied * len(basin), largest_three_basins, 1)

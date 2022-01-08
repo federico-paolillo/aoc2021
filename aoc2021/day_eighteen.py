631bc9d7e5ccacabfeb8ffe01b6384110369e50a
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2022-01-08 21:35:49 +0100

    Day Eighteen - Part 1 - Drafting

--- aoc2021/day_eighteen.py
+++ aoc2021/day_eighteen.py
@@ -1,141 +1,10 @@
 # NOTE: We are always ALWAYS implying that any snail number is well formed, we purposefully ignore any possible error
 
-def find_rightmost_node(current_node, visited=None):
-    if not current_node:
-        return None
-
-    if not visited:
-        visited = [current_node]
-
-    if current_node in visited:
-        return find_rightmost_node(current_node.parent, visited)
-
-    if isinstance(current_node.right, int):
-        return current_node
-
-    visited.append(current_node)
-
-    return find_rightmost_node(current_node.right, visited)
-
-
-class SnailfishNumber:
-    def __init__(self, left, right):
-        if left is None:
-            raise ValueError('A SnailfishNumber must always have a left')
-
-        self.left = left
-
-        if right is None:
-            raise ValueError('A SnailfishNumber must always have a right')
-
-        self.right = right
-
-        self._parent = None
-
-    @property
-    def left(self):
-        return self._left
-
-    @left.setter
-    def left(self, value):
-        self._left = value
-        if isinstance(self.left, SnailfishNumber):
-            self.left._parent = self
-
-    @property
-    def right(self):
-        return self._right
-
-    @right.setter
-    def right(self, value):
-        self._right = value
-        if isinstance(self.right, SnailfishNumber):
-            self.right._parent = self
-
-    # self.parent can be manipulated and assigned only through SnailNumber methods to avoid inconsistencies
-    # e.g.: How do you know if you are the left or right of your parent
-    # e.g.: What if you have a parent but your parent does not have you ?
-    # By design a SnailNumber is never nested until...it is nested somewhere
-    @property
-    def parent(self):
-        return self._parent
-
-    def is_reduced(self):
-        if self.can_split():
-            return False
-
-        if self.can_explode():
-            return False
-
-        if isinstance(self.left, SnailfishNumber):
-            if not self.left.is_reduced():
-                return False
-
-        if isinstance(self.right, SnailfishNumber):
-            if not self.right.is_reduced():
-                return False
-
-        return True
-
-    def can_split(self):
-        if isinstance(self.left, int):
-            can_split_left = self.left >= 10
-        else:
-            can_split_left = self.left.can_split()
-
-        if isinstance(self.right, int):
-            can_split_right = self.right >= 10
-        else:
-            can_split_right = self.right.can_split()
-
-        return can_split_left or can_split_right
-
-    def can_explode(self):
-        # If we have at least 4 parents it means we are 4 parenthesis deep
-        current_parent = self._parent
-        parents_count = 0
-
-        while current_parent:
-            current_parent = current_parent.parent
-            parents_count = parents_count + 1
-
-            if parents_count >= 4:
-                return True
-
-        return False
-
-    def __str__(self):
-        return f'[{self.left}, {self.right}]'
-
-    def __repr__(self):
-        return f'SnailfishNumber({repr(self.left)}, {repr(self.right)})'
-
-    # No __hash__, this class is mutable
-    def __eq__(self, other):
-        if not isinstance(other, SnailfishNumber):
-            return False
-        return other.left == self.left and other.right == self.right
-
-    def __add__(self, other):
-        if not isinstance(other, SnailfishNumber):
-            return NotImplemented
-        if not self.is_reduced():
-            raise ValueError(f'{self} is not reduced')
-        if not other.is_reduced():
-            raise ValueError(f'{other} is not reduced')
-        return SnailfishNumber(self, other)
-
-    def __radd__(self, other):
-        return self + other
-
-    def __iadd__(self, other):
-        self.left = SnailfishNumber(self.left, self.right)
-        self.right = other
-        return self
-
-
 # To find the separator index we search for the first separator that appears after all the 'left' part parenthesis have been closed
 # The comma that separates left from right can only appear after the left part is completely formed and 'closed'
+import math
+
+
 def _find_parts_separator_index(snailfish_number_string_without_parens):
     parens_stack = []
 
@@ -151,7 +20,57 @@ def _find_parts_separator_index(snailfish_number_string_without_parens):
     return None
 
 
+def _find_rightmost_regular_number(current_node, visited=None):
+    if not current_node:
+        return None
+
+    if not visited:
+        visited = [current_node]
+
+    if current_node in visited:
+        return _find_rightmost_regular_number(current_node.parent, visited)
+
+    if isinstance(current_node, RegularNumber):
+        return current_node
+
+    visited.append(current_node)
+
+    return _find_rightmost_regular_number(current_node.right, visited)
+
+
+def _find_leftmost_regular_number(current_node, visited=None):
+    if not current_node:
+        return None
+
+    if not visited:
+        visited = [current_node]
+
+    if current_node in visited:
+        return _find_leftmost_regular_number(current_node.parent, visited)
+
+    if isinstance(current_node, RegularNumber):
+        return current_node
+
+    visited.append(current_node)
+
+    return _find_leftmost_regular_number(current_node.left, visited)
+
+
+def _calculate_nesting(node):
+    parents_count = 0
+    current_parent = node.parent
+
+    while current_parent:
+        current_parent = current_parent.parent
+        parents_count = parents_count + 1
+
+    return parents_count
+
+
 def parse_snailfish_number(snailfish_number_string):
+    if snailfish_number_string.isnumeric():
+        return RegularNumber(int(snailfish_number_string))
+
     # Remove one char left and right, this should strip the number parenthesis ([])
     snailfish_number_string_without_parens = snailfish_number_string[1:-1]
 
@@ -164,14 +83,207 @@ def parse_snailfish_number(snailfish_number_string):
     left_part_string = snailfish_number_string_without_parens[:parts_separator_comma_index].strip()
     right_part_string = snailfish_number_string_without_parens[parts_separator_comma_index + 1:].strip()
 
-    if left_part_string.isnumeric():
-        left_part = int(left_part_string)
-    else:
-        left_part = parse_snailfish_number(left_part_string)
-
-    if right_part_string.isnumeric():
-        right_part = int(right_part_string)
-    else:
-        right_part = parse_snailfish_number(right_part_string)
+    left_part = parse_snailfish_number(left_part_string)
+    right_part = parse_snailfish_number(right_part_string)
 
     return SnailfishNumber(left_part, right_part)
+
+
+class RegularNumber:
+    def __init__(self, value):
+        self.value = value
+        self._parent = None
+
+    @property
+    def parent(self):
+        return self._parent
+
+    def can_split(self):
+        return self.value >= 10
+
+    def can_explode(self):
+        return False
+
+    def split(self):
+        if not self.can_split():
+            return
+
+        if not self.parent:
+            return
+
+        floor = math.floor(self.value / 2)
+        ceil = math.ceil(self.value / 2)
+
+        self.parent.replace(self, SnailfishNumber(floor, ceil))
+
+    def explode(self):
+        pass
+
+    def __repr__(self):
+        return f'RegularNumber({repr(self.value)})'
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        if not isinstance(other, RegularNumber):
+            return False
+
+        return self.value == other.value
+
+    def __add__(self, other):
+        if not isinstance(other, RegularNumber):
+            return NotImplemented
+
+        return RegularNumber(self.value + other.value)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        if not isinstance(other, RegularNumber):
+            return NotImplemented
+
+        self.value = self.value + other.value
+
+        return self
+
+
+class SnailfishNumber:
+    def __init__(self, left, right):
+        if left is None:
+            raise ValueError('A SnailfishNumber must always have a left')
+
+        if right is None:
+            raise ValueError('A SnailfishNumber must always have a right')
+
+        self.left = left
+        self.right = right
+
+        self._parent = None
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, value):
+        self._left = RegularNumber(value) if isinstance(value, int) else value
+        self.left._parent = self
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, value):
+        self._right = RegularNumber(value) if isinstance(value, int) else value
+        self.right._parent = self
+
+    # self.parent can be manipulated and assigned only through SnailNumber methods to avoid inconsistencies
+    # e.g.: How do you know if you are the left or right of your parent
+    # e.g.: What if you have a parent but your parent does not have you ?
+    # By design a SnailNumber is never nested until...it is nested somewhere
+    @property
+    def parent(self):
+        return self._parent
+
+    def can_reduce(self):
+        return self.can_split() or self.can_explode()
+
+    def can_split(self):
+        return self.left.can_split() or self.right.can_split()
+
+    def can_explode(self):
+        if self.is_explodable():
+            return True
+
+        return self.left.can_explode() or self.right.can_explode()
+
+    def is_explodable(self):
+        is_nested_in_more_than_four_pairs = _calculate_nesting(self) >= 4
+
+        left_is_regular_number = isinstance(self.left, RegularNumber)
+        right_is_regular_number = isinstance(self.right, RegularNumber)
+
+        return is_nested_in_more_than_four_pairs and (right_is_regular_number and left_is_regular_number)
+
+    def split(self):
+        if not self.can_split():
+            return
+
+        # Explode has precedence on split()
+        if self.can_explode():
+            return
+
+        self.left.split()
+        self.right.split()
+
+    def explode(self):
+        if not self.can_explode():
+            return
+
+        if self.is_explodable():
+            rightmost_regular_number = _find_rightmost_regular_number(self)
+
+            if rightmost_regular_number:
+                rightmost_regular_number += self.right
+
+            leftmost_regular_number = _find_leftmost_regular_number(self)
+
+            if leftmost_regular_number:
+                leftmost_regular_number += self.left
+
+            self.parent.replace(self, RegularNumber(0))
+
+        self.left.explode()
+        self.right.explode()
+
+    def replace(self, node, replacement_node):
+        if node.parent is not self:
+            return
+
+        if self.left is node:
+            self.left = replacement_node
+
+        if self.right is node:
+            self.right = replacement_node
+
+        node._parent = None
+
+    def reduce(self):
+        while self.can_reduce():
+            self.explode()
+            self.split()
+
+    def __str__(self):
+        return f'[{self.left}, {self.right}]'
+
+    def __repr__(self):
+        return f'SnailfishNumber({repr(self.left)}, {repr(self.right)})'
+
+    def __eq__(self, other):
+        if not isinstance(other, SnailfishNumber):
+            return False
+        return other.left == self.left and other.right == self.right
+
+    def __add__(self, other):
+        if not isinstance(other, SnailfishNumber):
+            return NotImplemented
+        if self.can_reduce():
+            raise ValueError(f'{self} is not reduced')
+        if other.can_reduce():
+            raise ValueError(f'{other} is not reduced')
+        return SnailfishNumber(self, other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        if not isinstance(other, SnailfishNumber):
+            return NotImplemented
+
+        self.left = SnailfishNumber(self.left, self.right)
+        self.right = other
+
+        return self
--- test/test_day_eighteen.py
+++ test/test_day_eighteen.py
@@ -1,6 +1,6 @@
 import unittest
 
-from aoc2021.day_eighteen import parse_snailfish_number, SnailfishNumber, find_rightmost_node
+from aoc2021.day_eighteen import parse_snailfish_number, SnailfishNumber
 
 
 class DayEighteenTestCase(unittest.TestCase):
@@ -10,7 +10,7 @@ class DayEighteenTestCase(unittest.TestCase):
 
         self.assertEqual(expected_snailfish_number, snailfish_number)
         self.assertEqual('[1, 2]', str(snailfish_number))
-        self.assertEqual('SnailfishNumber(1, 2)', repr(snailfish_number))
+        self.assertEqual('SnailfishNumber(RegularNumber(1), RegularNumber(2))', repr(snailfish_number))
 
     def test_parse_snailfish_number_parses_nested_number(self):
         snailfish_number = parse_snailfish_number('[[1,2], 3]')
@@ -18,11 +18,12 @@ class DayEighteenTestCase(unittest.TestCase):
 
         self.assertEqual(expected_snailfish_number, snailfish_number)
         self.assertEqual('[[1, 2], 3]', str(snailfish_number))
-        self.assertEqual('SnailfishNumber(SnailfishNumber(1, 2), 3)', repr(snailfish_number))
+        self.assertEqual('SnailfishNumber(SnailfishNumber(RegularNumber(1), RegularNumber(2)), RegularNumber(3))', repr(snailfish_number))
 
     def test_addition_sums_snailfish_numbers_correctly__add__(self):
         snailfish_number_a = SnailfishNumber(1, 2)
         snailfish_number_b = SnailfishNumber(SnailfishNumber(3, 4), 5)
+
         snailfish_number_sum = snailfish_number_a + snailfish_number_b
 
         self.assertEqual(SnailfishNumber(SnailfishNumber(1, 2), SnailfishNumber(SnailfishNumber(3, 4), 5)), snailfish_number_sum)
@@ -30,6 +31,7 @@ class DayEighteenTestCase(unittest.TestCase):
     def test_addition_sums_snailfish_numbers_correctly__radd__(self):
         snailfish_number_a = SnailfishNumber(SnailfishNumber(3, 4), 5)
         snailfish_number_b = SnailfishNumber(1, 2)
+
         snailfish_number_sum = snailfish_number_a + snailfish_number_b
 
         self.assertEqual(SnailfishNumber(SnailfishNumber(SnailfishNumber(3, 4), 5), SnailfishNumber(1, 2)), snailfish_number_sum)
@@ -37,6 +39,7 @@ class DayEighteenTestCase(unittest.TestCase):
     def test_addition_sums_snailfish_numbers_correctly__iadd__(self):
         snailfish_number_a = SnailfishNumber(1, 2)
         snailfish_number_b = SnailfishNumber(SnailfishNumber(3, 4), 5)
+
         snailfish_number_a += snailfish_number_b
 
         self.assertEqual(SnailfishNumber(SnailfishNumber(1, 2), SnailfishNumber(SnailfishNumber(3, 4), 5)), snailfish_number_a)
@@ -52,25 +55,44 @@ class DayEighteenTestCase(unittest.TestCase):
         self.assertFalse(snailfish_number.can_split())
 
     def test_can_explode_returns_True_for_explodable_snailfish_number(self):
-        explodable_snailfish_number = SnailfishNumber(9, 8)
-        container_snailfish_number = SnailfishNumber(SnailfishNumber(SnailfishNumber(SnailfishNumber(explodable_snailfish_number, 1), 2), 3), 4)
+        snailfish_number = SnailfishNumber(SnailfishNumber(SnailfishNumber(SnailfishNumber(SnailfishNumber(9, 8), 1), 2), 3), 4)
 
-        self.assertFalse(container_snailfish_number.is_reduced())
-        self.assertTrue(explodable_snailfish_number.can_explode())
+        self.assertTrue(snailfish_number.can_reduce())
+        self.assertTrue(snailfish_number.can_explode())
 
     def test_can_explode_returns_False_for_non_explodable_snailfish_number(self):
-        non_explodable_snailfish_number = SnailfishNumber(9, 8)
-        container_snailfish_number = SnailfishNumber(SnailfishNumber(SnailfishNumber(non_explodable_snailfish_number, 1), 2), 3)
+        snailfish_number = SnailfishNumber(SnailfishNumber(SnailfishNumber(1, 2), 3), 4)
 
-        self.assertTrue(container_snailfish_number.is_reduced())
-        self.assertFalse(non_explodable_snailfish_number.can_explode())
+        self.assertFalse(snailfish_number.can_reduce())
+        self.assertFalse(snailfish_number.can_explode())
 
-    def test_rightmost_node(self):
-        # snailfish_number = '[1, [[[[1, 2], 3], 4], 5]]'
-        # expected_number = '[2, [[[0, 5], 4], 5]]'
-        snailfish_number_source_node = SnailfishNumber(0, 5)
-        expected_rightmost_snailfish_number = SnailfishNumber(snailfish_number_source_node, 4)
-        snailfish_number = SnailfishNumber(2, SnailfishNumber(expected_rightmost_snailfish_number, 5))
-        rightmost_snailfish_number = find_rightmost_node(snailfish_number_source_node)
+    def test_splitting_splits_snailfish_number_correctly(self):
+        snailfish_number = parse_snailfish_number('[[[[0, 7], 4], [15, [0, 13]]], [1, 1]]')
+        snailfish_number_after_split = parse_snailfish_number('[[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]]')
 
-        self.assertIs(expected_rightmost_snailfish_number, rightmost_snailfish_number)
+        self.assertTrue(snailfish_number.can_split())
+
+        snailfish_number.split()
+
+        self.assertEqual(snailfish_number_after_split, snailfish_number)
+
+    def test_explode_explodes_snailfish_number_correctly(self):
+        snailfish_number = parse_snailfish_number('[[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]]')
+        snailfish_number_after_explode = parse_snailfish_number('[[[[0, 7], 4], [15, [0, 13]]], [1, 1]]')
+
+        self.assertTrue(snailfish_number.can_explode())
+
+        snailfish_number.explode()
+
+        self.assertEqual(snailfish_number_after_explode, snailfish_number)
+
+    def test_reduce_reduces_snailfish_number_correctly(self):
+        snailfish_number = parse_snailfish_number('[[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]]')
+        snailfish_number_after_reduce = parse_snailfish_number('[[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]]')
+
+        snailfish_number.reduce()
+
+        print(snailfish_number)
+        print(snailfish_number_after_reduce)
+
+        self.assertEqual(snailfish_number_after_reduce, snailfish_number)
